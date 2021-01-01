2c226238af8318a7b2ee731ac24f0f38b41357da
m-lima <marcelowind@gmail.com>
2021-01-01 10:56:18 +0000

    Make volumes readonly

--- build.sh
+++ build.sh
@@ -4,11 +4,12 @@ docker rm nginx
 docker create \
   --publish 80:80 \
   --publish 443:443 \
-  --volume /var/www/html/public:/var/www/static \
-  --volume browsify:/var/www/browsify \
-  --volume skull:/var/www/skull \
-  --volume sudoku:/var/www/sudoku \
-  --volume passer:/var/www/passer \
+  --volume /var/www/html/public:/var/www/static:ro \
+  --volume browsify:/var/www/browsify:ro \
+  --volume skull:/var/www/skull:ro \
+  --volume sudoku:/var/www/sudoku:ro \
+  --volume passer:/var/www/passer:ro \
+  --volume cloud:/var/www/cloud:ro \
   --net fly \
   --name nginx \
   nginx
