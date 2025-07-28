e215c1743e1b9eec6db6810c543c5f1bd485c85b
Marcelo Lima <m-lima@users.noreply.github.com>
2025-07-29 00:52:38 +0200

    Nixify

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "A nginx front-end for services";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.git-crypt
+          ];
+        };
+      }
+    );
+
+}
